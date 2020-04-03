58379be100313f6ef206a6c451c0adde7d20d3fb
michael <michael.gmeiner@witt-gruppe.eu>
2020-04-03 09:20:12 +0200

    renable reactive mongo NettyDriver customization

--- src/main/java/de/mgmeiner/examples/mongo/multitenancy/Application.java
+++ src/main/java/de/mgmeiner/examples/mongo/multitenancy/Application.java
@@ -2,8 +2,10 @@ package de.mgmeiner.examples.mongo.multitenancy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoReactiveAutoConfiguration.class, MongoReactiveDataAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/de/mgmeiner/examples/mongo/multitenancy/mongo/MongoConfiguration.java
+++ src/main/java/de/mgmeiner/examples/mongo/multitenancy/mongo/MongoConfiguration.java
@@ -1,8 +1,20 @@
 package de.mgmeiner.examples.mongo.multitenancy.mongo;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.connection.netty.NettyStreamFactoryFactory;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
@@ -11,6 +23,15 @@ import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.convert.NoOpDbRefResolver;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+/**
+ * Provides required basic required beans from
+ * - {@link org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration}
+ * - {@link MongoReactiveDataAutoConfiguration}
+ * <p>
+ * which need to provided as auto-configuration is disabled for reactive mongo.
+ * <p>
+ * The code is just copied from the respective spring AutoConfiguration classes.
+ */
 @Configuration
 public class MongoConfiguration {
 
@@ -27,5 +48,52 @@ public class MongoConfiguration {
     @ConditionalOnMissingBean(DataBufferFactory.class)
     public DefaultDataBufferFactory dataBufferFactory() {
         return new DefaultDataBufferFactory();
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass({SocketChannel.class, NioEventLoopGroup.class})
+    static class NettyDriverConfiguration {
+
+        @Bean
+        @Order(Ordered.HIGHEST_PRECEDENCE)
+        MongoConfiguration.NettyDriverMongoClientSettingsBuilderCustomizer nettyDriverCustomizer(
+                ObjectProvider<MongoClientSettings> settings) {
+            return new MongoConfiguration.NettyDriverMongoClientSettingsBuilderCustomizer(settings);
+        }
+    }
+
+    private static final class NettyDriverMongoClientSettingsBuilderCustomizer
+            implements MongoClientSettingsBuilderCustomizer, DisposableBean {
+
+        private final ObjectProvider<MongoClientSettings> settings;
+
+        private volatile EventLoopGroup eventLoopGroup;
+
+        private NettyDriverMongoClientSettingsBuilderCustomizer(ObjectProvider<MongoClientSettings> settings) {
+            this.settings = settings;
+        }
+
+        @Override
+        public void customize(MongoClientSettings.Builder builder) {
+            if (!isStreamFactoryFactoryDefined(this.settings.getIfAvailable())) {
+                NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+                this.eventLoopGroup = eventLoopGroup;
+                builder.streamFactoryFactory(
+                        NettyStreamFactoryFactory.builder().eventLoopGroup(eventLoopGroup).build());
+            }
+        }
+
+        @Override
+        public void destroy() {
+            EventLoopGroup eventLoopGroup = this.eventLoopGroup;
+            if (eventLoopGroup != null) {
+                eventLoopGroup.shutdownGracefully().awaitUninterruptibly();
+                this.eventLoopGroup = null;
+            }
+        }
+
+        private boolean isStreamFactoryFactoryDefined(MongoClientSettings settings) {
+            return settings != null && settings.getStreamFactoryFactory() != null;
+        }
     }
 }
